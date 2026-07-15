34b42891254ae12e6eca59b35c0b0813db519dd6
DuyHaCode <starplatinum3172005@gmail.com>
2026-07-16 01:15:03 +0700

    add chat in game

---
+++ Assets/_Scripts/Chat/ChatManager.cs
@@ -0,0 +1,28 @@
+﻿using Fusion;
+
+public class ChatManager : NetworkBehaviour
+{
+    public static ChatManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public override void Spawned()
+    {
+        base.Spawned();
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void RPC_SendChat(string playerName, string message)
+    {
+        RPC_ReceiveChat(playerName, message);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_ReceiveChat(string playerName, string message)
+    {
+        ChatUI.Instance.AddMessage(playerName, message);
+    }
+}
